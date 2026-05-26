442810294c3c68439762a08d01988114b80bec20
Glêsio Santos <glesioss@gmail.com>
2026-05-26 15:37:00 -0300

    inicializando projeto

---
+++ lib/produto.rb
@@ -0,0 +1,11 @@
+class Produto
+
+  attr_reader :titulo, :preco 
+
+  def initialize(titulo, preco)
+    @titulo = titulo
+    @preco  = preco
+  end
+
+end
+
---
+++ main.rb
@@ -0,0 +1,6 @@
+require_relative "lib/util"
+
+tela = Util.new
+
+tela.gerar_titulo "Sistema de Lanchonete"
+
